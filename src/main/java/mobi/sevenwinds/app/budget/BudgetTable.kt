cbc7b253fbaaeffa959c1d8542f1cf226b02b3d2
Gleb121region <71015316+Gleb121region@users.noreply.github.com>
2024-01-23 22:26:12 +0300

    add new field into entity budget

--- src/main/java/mobi/sevenwinds/app/budget/BudgetTable.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetTable.kt
@@ -1,5 +1,6 @@
 package mobi.sevenwinds.app.budget
 
+import mobi.sevenwinds.app.author.AuthorTable
 import org.jetbrains.exposed.dao.EntityID
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
@@ -10,6 +11,7 @@ object BudgetTable : IntIdTable("budget") {
     val month = integer("month")
     val amount = integer("amount")
     val type = enumerationByName("type", 100, BudgetType::class)
+    val authorId = reference("author_id", AuthorTable).nullable()
 }
 
 class BudgetEntity(id: EntityID<Int>) : IntEntity(id) {
@@ -19,8 +21,9 @@ class BudgetEntity(id: EntityID<Int>) : IntEntity(id) {
     var month by BudgetTable.month
     var amount by BudgetTable.amount
     var type by BudgetTable.type
+    var authorId by BudgetTable.authorId
 
     fun toResponse(): BudgetRecord {
-        return BudgetRecord(year, month, amount, type)
+        return BudgetRecord(year, month, amount, type, authorId?.value)
     }
 }
