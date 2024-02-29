35e71c0ee27b361468729162c2f786edabed256a
darg0ltz <xfoj@bk.ru>
2024-02-29 15:26:00 +0300

    feat: new fields in dto

--- src/main/java/mobi/sevenwinds/app/budget/BudgetApi.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetApi.kt
@@ -10,6 +10,7 @@ import com.papsign.ktor.openapigen.route.path.normal.get
 import com.papsign.ktor.openapigen.route.path.normal.post
 import com.papsign.ktor.openapigen.route.response.respond
 import com.papsign.ktor.openapigen.route.route
+import org.joda.time.DateTime
 
 fun NormalOpenAPIRoute.budget() {
     route("/budget") {
@@ -29,7 +30,10 @@ data class BudgetRecord(
     @Min(1900) val year: Int,
     @Min(1) @Max(12) val month: Int,
     @Min(1) val amount: Int,
-    val type: BudgetType
+    val type: BudgetType,
+    val authorId: Int? = null,
+    val authorName: String? = null,
+    val authorCreatedAt: DateTime? = null
 )
 
 data class BudgetYearParam(
