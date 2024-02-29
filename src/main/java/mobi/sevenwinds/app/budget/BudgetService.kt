eb4d30ad6a483b9e79d36c4d7d8527f4ce915879
darg0ltz <xfoj@bk.ru>
2024-02-29 14:04:10 +0300

    feat: add ordering, replace pagination

--- src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
+++ src/main/java/mobi/sevenwinds/app/budget/BudgetService.kt
@@ -2,6 +2,7 @@ package mobi.sevenwinds.app.budget
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -21,14 +22,17 @@ object BudgetService {
 
     suspend fun getYearStats(param: BudgetYearParam): BudgetYearStatsResponse = withContext(Dispatchers.IO) {
         transaction {
-            val query = BudgetTable
-                .select { BudgetTable.year eq param.year }
-                .limit(param.limit, param.offset)
-
+            val query = BudgetTable.select { BudgetTable.year eq param.year }
+                .orderBy(BudgetTable.month to SortOrder.ASC)
+                .orderBy(BudgetTable.amount to SortOrder.DESC)
             val total = query.count()
-            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
 
-            val sumByType = data.groupBy { it.type.name }.mapValues { it.value.sumOf { v -> v.amount } }
+            val sumByType = BudgetEntity.wrapRows(query)
+                .groupBy { it.type.name }
+                .mapValues { it.value.sumOf { v -> v.amount } }
+
+            query.limit(param.limit, offset = param.offset)
+            val data = BudgetEntity.wrapRows(query).map { it.toResponse() }
 
             return@transaction BudgetYearStatsResponse(
                 total = total,
